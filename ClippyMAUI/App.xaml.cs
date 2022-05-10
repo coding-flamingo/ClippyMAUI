2cbcea7bfa80e59a57f8885bc2a66704f67c89a6
igal <igalfsg@gmail.com>
2022-05-10 16:28:14 -0400

    Added solution

---
+++ ClippyMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ClippyMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ ClippyMAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ClippyMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
