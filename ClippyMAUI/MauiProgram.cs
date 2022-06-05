db289fb6534b5280c48c7e110135693bdb116569
igal <igalfsg@gmail.com>
2022-06-05 14:21:26 -0400

    upgraded to RC1

--- ClippyMAUI/MauiProgram.cs
+++ ClippyMAUI/MauiProgram.cs
@@ -9,15 +9,13 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.RegisterBlazorMauiWebView()
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-
-		builder.Services.AddBlazorWebView();
-		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddSingleton<WeatherForecastService>();
 		builder.Services.AddScoped<ClippyService>();
 		return builder.Build();
 	}
